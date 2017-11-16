17124d359890ed7cb843b0528ad97e7508e7e0cf
IEUser <iespar@hotmail.es>
2017-11-16 13:51:50 +0100

    First commit

---
+++ mytests/fourthTest.js
@@ -0,0 +1,32 @@
+var assert = require('assert');
+
+describe('Array', function() {
+    describe('#indexOf()', function() {
+        it('should return -1 when the value is not present', function() {
+            assert.equal(-1, [1,2,3].indexOf(4));
+        });
+    });
+});
+
+function sum(x,y) {
+  if (x==0) {
+      return 0
+  }
+  else {
+      return x + y
+  }
+}
+
+describe('Koko', function() {
+    describe('sum()', function() {
+        it('sum should add two numbers!!!!!', function() {
+            assert.equal(sum(5,10),15);
+        });
+    });
+});
+
+describe('Kiko', function() {
+    it('20 + 30 should be equal to 50', function() {
+        assert.equal(sum(20,30),50);
+    });
+});
