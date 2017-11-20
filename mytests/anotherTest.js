121343acb3c6cf98245f9d5248f512491354be90
Inaki Esparza <iespar@hotmail.es>
2017-11-20 17:53:20 +0100

    Comment from team to another test

--- mytests/anotherTest.js
+++ mytests/anotherTest.js
@@ -1,6 +1,7 @@
+// Comment from team to anotherTest
 var assert = require('assert');
 
-describe('Array', function() {
+describe('Array-team', function() {
     describe('#indexOf()', function() {
         it('should return -1 when the value is not present', function() {
             assert.equal(-1, [1,2,3].indexOf(4));
