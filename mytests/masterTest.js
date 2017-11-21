e542c6f669a30b754238d75f6eb579121d92656d
Inaki Esparza <iespar@hotmail.es>
2017-11-21 20:30:11 +0100

    Committing masterTest reworded
    Signed-off-by: Inaki Esparza <iespar@hotmail.es>

---
+++ mytests/masterTest.js
@@ -0,0 +1,40 @@
+var assert = require('assert');
+
+describe('Array-webstorm', function() {
+    describe('#indexOf()', function() {
+        it('should return -1 when the value is not present', function() {
+            assert.equal(-1, [1,2,3].indexOf(4));
+        });
+    });
+});
+
+function sum(x,y) {
+  if (x==0) {
+      return 0
+  }
+  else {
+      return x + y
+  }
+}
+
+describe('Koko', function() {
+    describe('sum()', function() {
+        it('sum should add two numbers!!!!!', function() {
+            assert.equal(sum(5,10),15);
+        });
+    });
+});
+
+describe('Kiko-pepe', function() {
+    it('120 + 30 should be equal to 150', function() {
+        assert.equal(sum(120,30),150);
+    });
+
+    it('220 + 230 should be equal to 450', function() {
+        assert.equal(sum(120,230),450);
+    });
+
+    it('50 + 75 should be equal to 125', function() {
+        assert.equal(sum(50,50),100);
+    });
+});
